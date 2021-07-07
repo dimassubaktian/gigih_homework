527da3b78761f89f4d4abe2d5bc5ea687e7a3866
dimassubaktian <dimas.subaktianto11@gmail.com>
2021-07-07 12:13:21 +0700

    first commit

---
+++ Person.rb
@@ -0,0 +1,60 @@
+class Person
+    attr_accessor :hitpoint
+    attr_reader :name, :attack_damage, :deflect
+    def initialize(name, hitpoint, attack_damage, deflect)
+        @name = name
+        @hitpoint = hitpoint
+        @attack_damage = attack_damage      
+        @deflect = deflect
+    end
+
+    def to_s
+
+        "#{@name} has #{@hitpoint} hitpoint and #{@attack_damage} attack damage"
+        
+    end
+
+    def attack(other_person)
+
+        r = rand(1..100)
+        # puts "#{r}" // to know what number show and check is the app work or not  :)
+        if r <= other_person.deflect && other_person.deflect > 0
+            puts "#{@name} attacks #{other_person.name} with #{@attack_damage} damage"
+            puts "#{other_person.name} deflects the attack"
+        else
+            other_person.take_damage(@attack_damage)
+
+            puts "#{@name} attacks #{other_person.name} with #{@attack_damage} damage"
+    
+            if other_person.hitpoint == 0
+                puts other_person
+                puts "#{other_person.name} dies"
+                exit
+            end
+        end
+ 
+    end
+
+    def take_damage(damage)
+        @hitpoint -= damage
+    end
+
+    
+end
+
+
+# un used code 
+    # hit_effect(other_person, attack_damage) 
+
+
+    # def hit_effect(person, damage)
+    #     person.hitpoint = person.hitpoint - damage        
+    # end
+
+    # def check_hitpoint(person)
+    #     if person.hitpoint > 0
+            
+    #     else
+            
+    #     end
+    # end
